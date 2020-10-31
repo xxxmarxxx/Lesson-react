98abaac9c56c2a5de1854f460d4c59a00c952124
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2020-10-31 15:34:28 +0100

    first commit app6

---
+++ App6.js
@@ -0,0 +1,11 @@
+class CheckBoxAgeConfirmation extends React.Component {
+  render() {
+    return (
+      <>
+        <h1>Kup bilet na horror roku</h1>
+        <input type="checkbox" id="age " />
+        <label htmlFor="age"></label>
+      </>
+    );
+  }
+}
