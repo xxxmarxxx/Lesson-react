9d77f2e98f3980ae9c03015eba4cff9aa5d6ad6d
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2020-10-31 16:44:19 +0100

    zakonczenie formularza

--- App6.js
+++ App6.js
@@ -1,25 +1,37 @@
 const PositveMessage = () => <p>Mozesz ogladac film. Zapraszamy</p>;
 const NegativeMessage = () => <p>Nie mozesz ogladac filmu jesli masz 16 lat!</p>;
 class CheckBoxAgeConfirmation extends React.Component {
-    state ={
-        isConfirmed : false,
-    }
+  state = {
+    isConfirmed: false,
+  };
 
-    handleCheckBoxChange =() => {
-        this.setState({
-            isConfirmed: !this.state.isConfirmed
-        })
+  handleCheckBoxChange = () => {
+    this.setState({
+      isConfirmed: !this.state.isConfirmed,
+    });
+  };
+
+  displayMessage = () => {
+    if (this.state.isConfirmed) {
+      return <PositveMessage />;
+    } else {
+      return <NegativeMessage />;
     }
+  };
 
   render() {
-      console.log(this.state.isConfirmed);
+    console.log(this.state.isConfirmed);
     return (
-
       <>
         <h1>Kup bilet na horror roku!</h1>
-        <input type="checkbox" id="age" onChange={this.handleCheckBoxChange} checked={this.state.isConfirmed} />
+        <input
+          type="checkbox"
+          id="age"
+          onChange={this.handleCheckBoxChange}
+          checked={this.state.isConfirmed}
+        />
         <label htmlFor="age">Mam co najmniej 16 lat</label>
-        
+        {this.displayMessage()}
       </>
     );
   }
