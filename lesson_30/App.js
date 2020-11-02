5ae8d37183a0f1c8b2f6bf3f377ad55c258a72ca
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2020-11-02 16:34:32 +0100

    commit lesson 30

---
+++ lesson_30/App.js
@@ -0,0 +1,39 @@
+const PositveMessage = () => <p>Mozesz ogladac film. Zapraszamy</p>;
+const NegativeMessage = () => <p className="negative">Nie mozesz ogladac filmu jesli masz 16 lat!</p>;
+class CheckBoxAgeConfirmation extends React.Component {
+  state = {
+    isConfirmed: false,
+  };
+
+  handleCheckBoxChange = () => {
+    this.setState({
+      isConfirmed: !this.state.isConfirmed,
+    });
+  };
+
+  displayMessage = () => {
+    if (this.state.isConfirmed) {
+      return <PositveMessage />;
+    } else {
+      return <NegativeMessage />;
+    }
+  };
+
+  render() {
+    console.log(this.state.isConfirmed);
+    return (
+      <>
+        <h1>Kup bilet na horror roku!</h1>
+        <input
+          type="checkbox"
+          id="age"
+          onChange={this.handleCheckBoxChange}
+          checked={this.state.isConfirmed}
+        />
+        <label htmlFor="age">Mam co najmniej 16 lat</label>
+        {this.displayMessage()}
+      </>
+    );
+  }
+}
+ReactDOM.render(<CheckBoxAgeConfirmation />, document.getElementById("root"));
