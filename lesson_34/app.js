aa466e71c78c6302b94259ff3df4eb4e7bdfa3c6
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2020-11-03 15:32:27 +0100

    Warunkowe dodanie przycisku i obsluga stanu magazynu (availableProducts) Wyszarzanie liczby = 0 CSS Destrukturyzacja

--- lesson_34/app.js
+++ lesson_34/app.js
@@ -1,10 +1,34 @@
-class App extends Component {
-    state = {}
-    render() { 
-        return ( 
-        <div>{}</div>
-         );
-    }
+// Warunkowe dodanie przycisku i obsluga stanu magazynu (availableProducts)
+// Wyszarzanie liczby = 0 CSS
+// destrukturyzacja
+
+class App extends React.Component {
+  state = {
+    availableProducts: 7,
+    shoppingCart: 5,
+  };
+
+  handleRemoveFromCart = () => {
+    this.setState({
+        shoppingCart: this.state.shoppingCart - 1,
+    });
+  };
+
+  handleAddToCart = () => {
+    this.setState({
+        shoppingCart: this.state.shoppingCart + 1,
+    });
+  };
+
+  render() {
+    return (
+      <>
+        <button disabled={this.state.shoppingCart ? false : true} onClick={this.handleRemoveFromCart}> - </button>
+        <span> {this.state.shoppingCart} </span>
+        <button disabled={this.state.shoppingCart === this.state.availableProducts ? true : false } onClick={this.handleAddToCart} > + </button>
+      </>
+    );
+  }
 }
- 
-ReactDOM.render(<App />, document.getElementById('root'))
+
+ReactDOM.render(<App />, document.getElementById("root"));
