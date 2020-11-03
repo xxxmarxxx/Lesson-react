5e8eb769372f70e67929368d8ed37d97696a6a6e
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2020-11-03 11:01:29 +0100

    lesson 31

--- lesson_31/App.js
+++ lesson_31/App.js
@@ -1,37 +1,54 @@
 const PositveMessage = () => <p>Mozesz ogladac film. Zapraszamy</p>;
-const NegativeMessage = () => <p className="negative">Nie mozesz ogladac filmu jesli masz 16 lat!</p>;
+const NegativeMessage = () => (
+  <p className="negative">Nie mozesz ogladac filmu jesli masz 16 lat!</p>
+);
 class Ticketshop extends React.Component {
   state = {
     isConfirmed: false,
+    isFormSubmitted: false,
   };
 
   handleCheckBoxChange = () => {
     this.setState({
       isConfirmed: !this.state.isConfirmed,
+      isFormSubmitted: false
     });
   };
 
+  handleFormSubmit = (e) => {
+    e.preventDefault()
+    if(!this.state.isFormSubmitted) {
+    this.setState ({
+      isFormSubmitted: true
+    })
+  }
+  }
+
   displayMessage = () => {
-    if (this.state.isConfirmed) {
-      return <PositveMessage />;
-    } else {
-      return <NegativeMessage />;
-    }
+    if (this.state.isFormSubmitted) {
+      if (this.state.isConfirmed) {
+        return <PositveMessage />;
+      } else {
+        return <NegativeMessage />;
+      }
+    } else {return null}
   };
 
   render() {
-    console.log(this.state.isConfirmed);
+    // console.log(this.state.isConfirmed);
     return (
       <>
         <h1>Kup bilet na horror roku!</h1>
-        <input
-          type="checkbox"
-          id="age"
-          onChange={this.handleCheckBoxChange}
-          checked={this.state.isConfirmed}
-        />
-        <label htmlFor="age">Mam co najmniej 16 lat</label>
-        <button>Kup bilet</button>
+        <form onSubmit={this.handleFormSubmit}>
+          <input
+            type="checkbox"
+            id="age"
+            onChange={this.handleCheckBoxChange}
+            checked={this.state.isConfirmed}
+          />
+          <label htmlFor="age">Mam co najmniej 16 lat</label>
+          <button type="submit">Kup bilet</button>
+        </form>
         {this.displayMessage()}
       </>
     );
