792482ce7e4ccc0b85e9986caf7674e144b63f8b
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2020-11-03 17:34:24 +0100

    update index.html and style.css FINISH

--- lesson_34/app.js
+++ lesson_34/app.js
@@ -10,36 +10,58 @@ class App extends React.Component {
 
   handleRemoveFromCart = () => {
     this.setState({
-        shoppingCart: this.state.shoppingCart - 1,
+      shoppingCart: this.state.shoppingCart - 1,
     });
   };
 
   handleAddToCart = () => {
     this.setState({
-        shoppingCart: this.state.shoppingCart + 1,
+      shoppingCart: this.state.shoppingCart + 1,
     });
   };
 
-  handleBuy =() => {
-      this.setState({
-          availableProducts: this.state.availableProducts - this.state.shoppingCart, shoppingCart:0,
-      })
-      console.log("kupione");
-  }
+  handleBuy = () => {
+    this.setState({
+      availableProducts: this.state.availableProducts - this.state.shoppingCart,
+      shoppingCart: 0,
+    });
+    console.log("kupione");
+  };
 
   render() {
-    const {shoppingCart, availableProducts} = this.state
-    const {handleAddToCart, handleBuy, handleRemoveFromCart} = this
-    const style = shoppingCart === 0 ? {opacity: 0.3} : {};
+    const { shoppingCart, availableProducts } = this.state;
+    const { handleAddToCart, handleBuy, handleRemoveFromCart } = this;
+    const style = shoppingCart === 0 ? { opacity: 0.3 } : {};
 
     return (
       <>
-        <button disabled={shoppingCart ? false : true} onClick={handleRemoveFromCart}> - </button>
+        <h1>Lesson 34-35-36-37</h1>
+        <button
+          disabled={shoppingCart ? false : true}
+          onClick={handleRemoveFromCart}
+        >
+          {" "}
+          -{" "}
+        </button>
 
         <span style={style}> {shoppingCart} </span>
 
-        <button disabled={shoppingCart === availableProducts ? true : false } onClick={handleAddToCart} > + </button>
+        <button
+          disabled={shoppingCart === availableProducts ? true : false}
+          onClick={handleAddToCart}
+        >
+          {" "}
+          +{" "}
+        </button>
         {shoppingCart > 0 && <button onClick={handleBuy}>kup</button>}
+        <h4>
+          - Warunkowe dodanie przycisku i obsluga stanu magazynu
+          (availableProducts) 
+          <br />
+          - Wyszarzanie liczby = 0 CSS 
+          <br />
+          - destrukturyzacja
+        </h4>
       </>
     );
   }
