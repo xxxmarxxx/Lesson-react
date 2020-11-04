2d569241e31b1b5be5cb70244a546703c5a8d423
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2020-11-04 10:24:20 +0100

    first commit lesson 38/39

--- lesson_38/app.js
+++ lesson_38/app.js
@@ -1,11 +1,16 @@
 class ListItems extends React.Component {
    state={
-       items:["jablko", "sliwki", "gruszka"]
+       items:["jablko", "sliwki", "gruszka",'orange']
+       
    }
     render() { 
         return ( 
-            <h1>hallo world</h1>
-
+            <>
+            <h1>Lesson 38/39 - praca z tablicami</h1>
+            <ul>
+                {this.state.items.map((item) => <li key={item}> {`owoc ${item}`} </li>)}
+            </ul>
+            </>
          );
     }
 }
