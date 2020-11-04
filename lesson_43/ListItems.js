fdc52c1be84c92bda00858db216fc0dbf231eaa8
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2020-11-05 00:23:34 +0100

    ListItems.js  add  items to ul

--- lesson_43/ListItems.js
+++ lesson_43/ListItems.js
@@ -1,12 +1,17 @@
 const ListItems = (props)=> {
+    const items = props.items.map(item => (
+    <Item 
+    key={item.id}
+    name={item.name}
+    active={item.active}
+    />
+    ))
     return (
+        <div className="list">
+        <h1>Twoje Zamowienie</h1>
         <ul>
-            <li>1</li>
-            <li>2</li>
-            <li>3</li>
-            <li>4</li>
-            <li>5</li>
-            <li>6</li>
+           {items}
         </ul>
+        </div>
     )
 }
