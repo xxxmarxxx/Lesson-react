67db26a9054e54b7b1f3209fdbddc62becf63b5d
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2020-11-04 10:38:44 +0100

    create const Items

--- lesson_38/app.js
+++ lesson_38/app.js
@@ -1,14 +1,19 @@
+
+const Item = (props) => <li>{`owoc ${props.content}`}</li>
+
 class ListItems extends React.Component {
    state={
        items:["jablko", "sliwki", "gruszka",'orange']
        
    }
     render() { 
+        const Items = this.state.items.map(item => <Item key={item} content={item} />)
         return ( 
+            
             <>
             <h1>Lesson 38/39 - praca z tablicami</h1>
             <ul>
-                {this.state.items.map((item) => <li key={item}> {`owoc ${item}`} </li>)}
+                {Items}
             </ul>
             </>
          );
