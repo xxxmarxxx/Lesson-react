5f74c1feb64c29ead4e889f29ef06f201721a7a5
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2020-11-04 23:41:52 +0100

    create object with items

--- lesson_43/App.js
+++ lesson_43/App.js
@@ -7,14 +7,24 @@
 
 
 class App extends React.Component {
-  state = {}
+  state = {
+    items: [
+    { id: 1, name: "herbata", active: false},
+    { id: 2, name: "ziemniaki", active: false},
+    { id: 3, name: "kasza", active: false},
+    { id: 4, name: "zupa wodna", active: false},
+    { id: 5, name: "wrzatek", active: false},
+    { id: 5, name: "chleb", active: false},
+  ],
+
+  }
   render() { 
     return ( 
-      <>
-      <h1>Lession 43 interaktywne menu dla restouracji cz.1</h1>
-      <h2>Menu</h2>
-      </>
+      <React.Fragment>
+      <h1>Lession 43 interaktywne menu dla restauracji cz.1</h1>
+      <Header items={this.state.items} />
+      <ListItems />
+      </React.Fragment>
      );
   }
 }
-
