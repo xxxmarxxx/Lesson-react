07947aedb2ddba107b0d46e8d60fa5724f98030e
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2020-11-05 12:18:01 +0100

    create form cz.1

--- lesson_47/App.js
+++ lesson_47/App.js
@@ -1,26 +1,70 @@
 // import React from "./przycisk";
 
 class Form extends React.Component {
+  state = {
+    city: "London",
+    text: "",
+    isLoved: true,
+    number: "0"
+   }
+
+handleCityChange = (e) => {
+  this.setState({
+    city: e.target.value,
+  })
+}
+handleTextChange = (e) => {
+  this.setState({
+    text: e.target.value,
+    
+  })
+}
+
+handleIsLovedChange = (e) => {
+  this.setState({
+isLoved: e.target.checked,
+  })
+}
+
+handleVisitNumberChange (e) {
+  this.setState({
+number: e.target.value,
+  })
+}
+
   render() {
     return (
       <>
       <h3 className="title">
-          Witaj na lekcji 47 komponent kontrolowany i niekontrolowany
+          Witaj na lekcji 47 cz.1 komponent kontrolowany i niekontrolowany
         </h3>
-      <form>
-        
         <label>
-          Podaj imie:
-          <input type="text" name="name" />
+          Podaj miasto
+          <input value={this.state.city} onChange={this.handleCityChange} type="text"/>
         </label>
-        <br />
+        <br/>
         <label>
-          Podaj email:
-          <input type="email" name="email" />
+          Napisz mi cos o tym miescie
+          <textarea value={this.state.text} onChange={this.handleTextChange} type="text"/>
         </label>
-        <br />
-        <button>Zapisz sie</button>
-      </form>
+        <br/>
+        <label>
+          czy lubisz to miasto?
+          <input type="checkbox" checked={this.state.isLoved}
+          onChange={this.handleIsLovedChange} />
+        </label>
+        <br/>
+        <label>
+          Ile razy byliscie w tym miescie?
+          <select value={this.state.number} onChange={this.handleVisitNumberChange.bind(this)}>
+            <option value="0">0</option>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="more">wiecej</option>
+          </select>
+        </label>
+      
       </>
     );
   }
