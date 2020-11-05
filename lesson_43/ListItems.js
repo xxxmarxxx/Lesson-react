aacfd06b01fec3d076009c773b77aea83277fd58
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2020-11-05 01:23:45 +0100

    add id={item.id} and changeStatus={props.changeStatus}

--- lesson_43/ListItems.js
+++ lesson_43/ListItems.js
@@ -2,8 +2,10 @@ const ListItems = (props)=> {
     const items = props.items.map(item => (
     <Item 
     key={item.id}
+    id={item.id}
     name={item.name}
     active={item.active}
+    changeStatus={props.changeStatus}
     />
     ))
     return (
