4908b8fbbfc7e7b9bd1d30586ee78175d240723f
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2020-11-05 16:12:13 +0100

    simplify 3 components into one app,js

--- lesson_49/app.js
+++ lesson_49/app.js
@@ -2,33 +2,35 @@
 
 
 // wersja 1 dollar na 5.11.20
-const Dollars = (props)=> (
-<div>Wartosc w dolarach: <strong>{props.cash <= 0 ? "" : (props.cash / props.ratio).toFixed(2)}</strong></div>
-)
-
+// const Dollars = (props)=> (
+// <div>Wartosc w dolarach: <strong>{props.cash <= 0 ? "" : (props.cash / props.ratio).toFixed(2)}</strong></div>
+// )
 
 //wersja 2 Euro na 5.11.20
-const Euros = (props)=> {
-        const value = props.cash <= 0 ? "" : (props.cash / props.ratio).toFixed(2)
-        return (
-        <div>Wartosc w euro: <strong>{value}</strong></div>
-    )
-}
+// const Euros = (props)=> {
+//         const value = props.cash <= 0 ? "" : (props.cash / props.ratio).toFixed(2)
+//         return (
+//         <div>Wartosc w euro: <strong>{value}</strong></div>
+//     )
+// }
 //wersja 3 chinska waluta na 5.11.20
-const Yuan = (props)=> {
-    const value = (props.cash * props.ratio).toFixed(2)
-    return (
-    <div>Wartosc w Yuan: <strong>{props.cash <= 0 ? "" : value}</strong></div>
-)
-}
+// const Yuan = (props)=> {
+//     const value = (props.cash / props.ratio).toFixed(2)
+//     return (
+//     <div>Wartosc w Yuan: <strong>{props.cash <= 0 ? "" : value}</strong></div>
+// )
+// }
 
+const Cash = (props)=> (
+<div> {props.title} <strong>{props.cash <= 0 ? "" : (props.cash / props.ratio).toFixed(2)}</strong></div>
+)
 
 class ExchangeCounter extends React.Component {
   state = {
         amount: "",
         ratioDollar: 3.81,
         ratioEuro: 4.52,
-        ratioYuan: 1.73,
+        ratioYuan: 0.58,
 
   };
 
@@ -56,9 +58,12 @@ class ExchangeCounter extends React.Component {
             />
           </label>
           <div className="cash">
-          <Dollars cash={amount} ratio={ratioDollar}/>
-          <Euros cash={amount}ratio={ratioEuro}/>
-          <Yuan cash={amount}ratio={ratioYuan}/>
+          {/* <Dollars cash={amount} ratio={ratioDollar}/> */}
+          {/* <Euros cash={amount}ratio={ratioEuro}/> */}
+          {/* <Yuan cash={amount}ratio={ratioYuan}/> */}
+          <Cash cash={amount} ratio={ratioDollar} title="Wartosc w dollarach:"/>
+          <Cash cash={amount} ratio={ratioEuro} title="Wartosc w Euro:"/>
+          <Cash cash={amount} ratio={ratioYuan} title="Wartosc w Yuan:"/>
           </div>
         </div>
       </>
