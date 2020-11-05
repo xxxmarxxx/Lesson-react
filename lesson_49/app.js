6b1ca6670bbe65a668d96d0ef2bd5371396c0ba6
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2020-11-05 15:51:16 +0100

    update style.css and app.js

--- lesson_49/app.js
+++ lesson_49/app.js
@@ -1,31 +1,65 @@
 // kantor walut
 
-const Dollars = ()=> (
-    
-    <div>Wartosc w dolarach:</div>
+
+// wersja 1 dollar na 5.11.20
+const Dollars = (props)=> (
+<div>Wartosc w dolarach: <strong>{props.cash <= 0 ? "" : (props.cash / props.ratio).toFixed(2)}</strong></div>
 )
 
-const Euros = ()=> (
-    <div>Wartosc w euro:</div>
+
+//wersja 2 Euro na 5.11.20
+const Euros = (props)=> {
+        const value = props.cash <= 0 ? "" : (props.cash / props.ratio).toFixed(2)
+        return (
+        <div>Wartosc w euro: <strong>{value}</strong></div>
+    )
+}
+//wersja 3 chinska waluta na 5.11.20
+const Yuan = (props)=> {
+    const value = (props.cash * props.ratio).toFixed(2)
+    return (
+    <div>Wartosc w Yuan: <strong>{props.cash <= 0 ? "" : value}</strong></div>
 )
+}
+
 
 class ExchangeCounter extends React.Component {
   state = {
-        amount: ""
+        amount: "",
+        ratioDollar: 3.81,
+        ratioEuro: 4.52,
+        ratioYuan: 1.73,
+
   };
-  
+
+  handleChange = (e) => {
+      this.setState({
+          amount: e.target.value
+      })
+  }
+
   render() {
+
+    const {amount, ratioDollar, ratioEuro, ratioYuan} = this.state;
+    // destrukturyzacja
     return (
       <>
         <div className="app">
           <h3 className="title">Witaj na lekcji 49 cz.1 Kantor walut</h3>
 
-          <h2 className="">Kantor</h2>
+          <h2 className="">Kantor Walut</h2>
           <label>
-            <input type="number" />
+            <input 
+            type="number" 
+            value={amount} 
+            onChange={this.handleChange}
+            />
           </label>
-          <Dollars />
-          <Euros />
+          <div className="cash">
+          <Dollars cash={amount} ratio={ratioDollar}/>
+          <Euros cash={amount}ratio={ratioEuro}/>
+          <Yuan cash={amount}ratio={ratioYuan}/>
+          </div>
         </div>
       </>
     );
