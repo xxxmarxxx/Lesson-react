21d5bfa1ec595b44d5df1e49a2d30b636008008a
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2020-11-05 17:04:22 +0100

    upgrade 49/50 lesson

--- lesson_49/app.js
+++ lesson_49/app.js
@@ -1,75 +1,75 @@
-// kantor walut
+// kantor walut na dzien 5.11.2020
 
-
-// wersja 1 dollar na 5.11.20
-// const Dollars = (props)=> (
-// <div>Wartosc w dolarach: <strong>{props.cash <= 0 ? "" : (props.cash / props.ratio).toFixed(2)}</strong></div>
-// )
-
-//wersja 2 Euro na 5.11.20
-// const Euros = (props)=> {
-//         const value = props.cash <= 0 ? "" : (props.cash / props.ratio).toFixed(2)
-//         return (
-//         <div>Wartosc w euro: <strong>{value}</strong></div>
-//     )
-// }
-//wersja 3 chinska waluta na 5.11.20
-// const Yuan = (props)=> {
-//     const value = (props.cash / props.ratio).toFixed(2)
-//     return (
-//     <div>Wartosc w Yuan: <strong>{props.cash <= 0 ? "" : value}</strong></div>
-// )
-// }
-
-const Cash = (props)=> (
-<div> {props.title} <strong>{props.cash <= 0 ? "" : (props.cash / props.ratio).toFixed(2)}</strong></div>
-)
-
-
+const Cash = (props) => {
+  const value = (props.cash / props.ratio).toFixed(2);
+  return (
+    <div>
+      {props.title} {props.cash <= 0 ? "" : value}
+    </div>
+  );
+};
 
 class ExchangeCounter extends React.Component {
   state = {
-        amount: "",
-        ratioDollar: 3.81,
-        ratioEuro: 4.52,
-        ratioYuan: 0.58,
-
+    amount: "",
   };
 
+  currencies = [
+    {
+      id: 1,
+      name: "dollar",
+      ratio: 3.81,
+      title: "Wartosc w dollarach:",
+    },
+    {
+      id: 2,
+      name: "euro",
+      ratio: 4.52,
+      title: "Wartosc w euro:",
+    },
+    {
+      id: 3,
+      name: "dollar",
+      ratio: 0.58,
+      title: "Wartosc w chinskich Yuan:",
+    },
+    {
+      id: 4,
+      name: "pound",
+      ratio: 5.0,
+      title: "Wartosc w funtach:",
+    },
+  ];
+
   handleChange = (e) => {
-      this.setState({
-          amount: e.target.value
-      })
-  }
+    this.setState({
+      amount: e.target.value,
+    });
+  };
 
   render() {
-
-    const {amount, ratioDollar, ratioEuro, ratioYuan} = this.state;
+    const { amount } = this.state;
     // destrukturyzacja
+
+    const calculators = this.currencies.map((currency) => (
+      <Cash
+        key={currency.id}
+        ratio={currency.ratio}
+        title={currency.title}
+        cash={amount}
+      />
+    ));
+
     return (
       <>
         <div className="app">
-          <h3 className="title">Witaj na lekcji 49 cz.1 Kantor walut</h3>
+          <h3 className="title">Witaj na lekcji 49/50 Kantor walut</h3>
 
           <h2 className="">Kantor Walut</h2>
           <label>
-            <input 
-            type="number" 
-            value={amount} 
-            onChange={this.handleChange}
-            />
+            <input type="number" value={amount} onChange={this.handleChange} />
           </label>
-          <div className="cash">
-              
-          {/* <Dollars cash={amount} ratio={ratioDollar}/> */}
-          {/* <Euros cash={amount}ratio={ratioEuro}/> */}
-          {/* <Yuan cash={amount}ratio={ratioYuan}/> */}
-
-          {/* <Cash cash={amount} ratio={ratioDollar} title="Wartosc w dollarach:"/>
-          <Cash cash={amount} ratio={ratioEuro} title="Wartosc w Euro:"/>
-          <Cash cash={amount} ratio={ratioYuan} title="Wartosc w Yuan:"/> */}
-
-          </div>
+          <div className="cash">{calculators}</div>
         </div>
       </>
     );
