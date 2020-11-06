802ba2c7ca64ab8d8e5107bbde6b6c50e0e8b632
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2020-11-06 19:20:24 +0100

    add

--- zadania/Usuwanie_elementow_listy/app.js
+++ zadania/Usuwanie_elementow_listy/app.js
@@ -1,25 +1,39 @@
+const Person = (props) => {
+    return(
+    <li>
+        <span>{props.name}</span>
+        <button onClick={props.delete}>Usun</button>
+    </li>
 
-
-class List extends React.Component{
-    state = {
-        people:[
-            { id: 1, name: "Jan K."},
-            { id: 2, name: "Piotr T."},
-            { id: 3, name: "Mariusz O."},
-            { id: 4, name: "Pawel K."},
-        ]
-    }
-
-    render(){
-    
-        return(
-            <>
-            <h1>Zadanie - usuwanie elementu listy</h1>
-        <ul>{this.state.people.map(person => <li key={person.id}>{person.name}</li>)}</ul>
-
-            </>
-        )
-    }
-
+    )
 }
-ReactDOM.render(<List />, document.getElementById('root'));
+
+class List extends React.Component {
+  state = {
+    people: [
+      { id: 1, name: "Jan K." },
+      { id: 2, name: "Piotr T." },
+      { id: 3, name: "Mariusz O." },
+      { id: 4, name: "Pawel K." },
+    ],
+  };
+
+  handleDelete() {
+        console.log("klikniete");
+  }
+
+  render() {
+    return (
+      <>
+      <section>
+        <h1>Zadanie - usuwanie elementu listy</h1>
+        <ul>
+          {this.state.people.map((person) => <Person key={person.id} 
+          name={person.name} delete={this.handleDelete.bind(this, person.id)}/>)}
+        </ul>
+        </section>
+      </>
+    );
+  }
+}
+ReactDOM.render(<List />, document.getElementById("root"));
