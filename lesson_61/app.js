8011c9d116f7c56d373ebbb745ee374cf77b71ef
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2020-11-09 12:51:20 +0100

    upgrade app.js

--- lesson_61/app.js
+++ lesson_61/app.js
@@ -11,6 +11,7 @@ class Clicker extends React.Component {
     });
     //console.log(this.state.number1 + "w metodzie handleClick");
   };
+
   handleClick2 = () => {
     this.setState({
       number2: this.state.number2 + 1,
@@ -23,29 +24,39 @@ class Clicker extends React.Component {
     });
     //  console.log(this.state.number2 + "w metodzie handleClick");
   };
+
   handleClick3 = () => {
     this.setState((state) => {
+        // console.log(state);
       return {
         number3: this.state.number3 + 1,
       };
     });
-    this.setState(() => ({
-      number3: this.state.number3 + 1,
-    }));
-    this.setState(() => ({
-      number3: this.state.number3 + 1,
-    }));
-    console.log(this.state.number3 + "w metodzie handleClick");
+    this.setState((state) => {
+        // console.log(state);
+        return {
+          number3: this.state.number3 + 1,
+        };
+      });
+      this.setState((prevState) => {
+        console.log(prevState);
+        console.log(this.state);
+        return {
+          number3: this.state.number3 + 1,
+        };
+      });
+    
+    console.log(this.state.number3);
   };
   render() {
     console.log(this.state.number1 + "w render");
     return (
       <>
-        <button onClick={this.handleClick1}>Podbil o 1</button>
+        <button onClick={this.handleClick1}>Podbij o 1</button>
         <h1>{this.state.number1}</h1>
-        <button onClick={this.handleClick2}>Podbil o 3</button>
+        <button onClick={this.handleClick2}>Podbij o 3</button>
         <h1>{this.state.number2}</h1>
-        <button onClick={this.handleClick3}>Podbil o 3</button>
+        <button onClick={this.handleClick3}>Podbij o 3</button>
         <h1>{this.state.number3}</h1>
       </>
     );
