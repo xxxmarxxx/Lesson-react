a1da8f346ab300ae069bd7776dae64e0c9758293
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2020-11-09 16:27:38 +0100

    getTime

--- lesson_65_Zegarek/app.js
+++ lesson_65_Zegarek/app.js
@@ -1,13 +1,26 @@
 class Clock extends React.Component {
   state = {
-  
+      time: {
+        hours: 12,
+        minutes: 34,
+        seconds: 15,
+      }
   };
 
+  getTime(){
+    const currentTime = new Date();
+    console.log(currentTime);
+  }
+
   render() {
+    this.getTime()
+    const {hours,minutes,seconds} = this.state.time
     return (
       <>
       <h1>Lesson 65 Zegarek</h1>
-      
+      <div className="clock">
+      {hours} : {minutes} : {seconds}
+      </div>
       
 
       </>
