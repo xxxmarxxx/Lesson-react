d67c5b808698f93f01fc7c97c66231647b34d749
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2020-11-09 15:10:25 +0100

    first commit lesson 64

---
+++ lesson_64_Kalkulator/app.js
@@ -0,0 +1,17 @@
+class App extends React.Component {
+  state = {
+    
+  };
+
+  render() {
+    return (
+      <>
+      <h1>Lesson 64 Kalkulator</h1>
+      <p>Usuwanie componentu </p>
+      
+      </>
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
