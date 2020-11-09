38f7a4bd7f608451d419378abd4d2691ad836324
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2020-11-09 13:40:57 +0100

    first commit lesson 62

---
+++ lesson_62/app.js
@@ -0,0 +1,19 @@
+class App extends React.Component {
+  state = {
+   
+  };
+
+  
+  
+  render() {
+    
+    return (
+      <>
+      <h1>Lesson 62 - Cykl zycia </h1>
+        
+      </>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
