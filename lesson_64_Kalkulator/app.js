29fe88d22afc9b731bee444db9f7a8c72c364108
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2020-11-09 15:26:45 +0100

    add button and result , ratio

--- lesson_64_Kalkulator/app.js
+++ lesson_64_Kalkulator/app.js
@@ -1,5 +1,7 @@
 class App extends React.Component {
   state = {
+    result:1,
+    ratio: 2,
     
   };
 
@@ -8,6 +10,8 @@ class App extends React.Component {
       <>
       <h1>Lesson 64 Kalkulator</h1>
       <p>Kalkulator mnozy przez dwa, jesli suma jest mniejsza niz 1000. Po przekroczeniu 1000 Kalkulator mnozy prez 0.5 az osiagnie sume mniejsza niz jeden. Wtedy ponownie zaczyna mnozyc przez dwa. </p>
+      <button>{`Pomoz przez ${this.state.ratio}`}</button>
+      <h1>Wynik</h1>
 
       </>
     )
