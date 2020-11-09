3a790fb73d780f980e3e8bf3a1bd3c9ac30e9fa1
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2020-11-09 14:19:53 +0100

    first commit lesson 63

---
+++ lesson_63/app.js
@@ -0,0 +1,20 @@
+class App extends React.Component {
+  state = {
+
+  }
+
+  render(){
+    return(
+      <>
+<h1>Lesson 63 Cykl zycia Button</h1>
+<p>Usuwanie componentu </p>
+
+
+      </>
+    )
+
+
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
