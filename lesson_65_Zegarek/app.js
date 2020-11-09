5caba128b5a5f4c1621e45aae7334afdb211019c
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2020-11-09 16:06:35 +0100

    first commit lesson 65

---
+++ lesson_65_Zegarek/app.js
@@ -0,0 +1,18 @@
+class Clock extends React.Component {
+  state = {
+  
+  };
+
+  render() {
+    return (
+      <>
+      <h1>Lesson 65 Zegarek</h1>
+      
+      
+
+      </>
+    )
+  }
+}
+
+ReactDOM.render(<Clock />, document.getElementById("root"));
---
+++ lesson_65_Zegarek/readme.md
@@ -0,0 +1,2 @@
+## Lesson 65 - Aplikacja Zegarek
+* 
