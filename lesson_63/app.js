2fd447aefee87f9875819be85e910f49807edfbc
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2020-11-09 14:58:12 +0100

    create component child1 , child2

--- lesson_63/app.js
+++ lesson_63/app.js
@@ -1,19 +1,61 @@
 class App extends React.Component {
   state = {
+    status: true,
+  };
 
+  componentDidMount() {
+    console.log("Aplikacja zamontowana");
+  }
+  componentDidUpdate() {
+    console.log("Aplikacja aktualizowana");
   }
 
-  render(){
-    return(
+  render() {
+    console.log("renderowanie aplikacji");
+    return (
       <>
-<h1>Lesson 63 Cykl zycia Button</h1>
-<p>Usuwanie componentu </p>
-
-
+        <h1>Lesson 63 Cykl zycia Button</h1>
+        <p>Usuwanie componentu </p>
+        <button
+          onClick={() =>
+            this.setState({
+              status: !this.state.status,
+            })
+          }
+        >
+          Przelacz
+        </button>
+        <Child1 status={this.state.status} />
+        {this.state.status && <Child2 />}
       </>
-    )
+    );
+  }
+}
 
+class Child1 extends React.Component {
+  componentDidMount() {
+    console.log("Child1 zamontowana");
+  }
+  componentDidUpdate() {
+    console.log("Child1 aktualizowana");
+  }
+  render() {
+    console.log("renderowanie Child1");
+    return <div>{String(this.props.status)}</div>;
+  }
+}
 
+class Child2 extends React.Component {
+  componentDidMount() {
+    console.log("Child2 zamontowana");
+  }
+  componentDidUpdate() {
+    console.log("Child2 aktualizowana");
+  }
+
+  state = {};
+  render() {
+    return <div>Komponent Child2 zamontowany</div>;
   }
 }
 
