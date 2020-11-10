bf4e2018f94f439ca40347c19d17afa1b4549260
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2020-11-10 13:25:18 +0100

    finish project Stopper

--- lesson_69_install_react/example/src/components/App.js
+++ lesson_69_install_react/example/src/components/App.js
@@ -1,17 +1,45 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import './App.css';
+import SwitchButton from './SwitchButton'
+
 
 
 class App extends Component {
-  
+  state = { 
+    time: 0,
+    active: false
+   }
+
+handleClick = () => {
+  console.log('klick');
+  if(this.state.active){
+    clearInterval(this.idInterval)
+  } else {
+    this.idInterval = setInterval(() => this.addSecond(), 1000)
+  }
+
+this.setState({
+  active:!this.state.active
+})
+}
+
+addSecond = ()=> {
+  this.setState({
+    time: this.state.time + 1
+  })
+}
+
   render() { 
     return ( 
-      <div className="app">
-        Aplikacja
-      </div>
+      <>
+      <p>{this.state.time}</p>
+      <SwitchButton click={this.handleClick} active={this.state.active} />
+      </>
      );
   }
 }
  
 export default App;
+
+
 
