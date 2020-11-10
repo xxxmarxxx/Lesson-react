4689c390599b61db32fc7c847cf2a46091869f82
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2020-11-10 16:59:59 +0100

    finish project

--- lesson_74_pobieranieDanych/app/src/App.js
+++ lesson_74_pobieranieDanych/app/src/App.js
@@ -3,42 +3,57 @@ import "./App.css";
 
 // pseudo rosnaca baza danych
 const data = [
-  {
-    id: 1,
-    title: "Wiadomosc numer 1",
-    body: "Zawartosc wiadomosci numer 1...",
-  },
-  {
-    id: 2,
-    title: "Wiadomosc numer 2",
-    body: "Zawartosc wiadomosci numer 2...",
-  },
-];
+  { id: 1, title: "Wiadomosc numer 1", body: "Zawartosc wiadomosci numer 1 ..." },
+  { id: 2, title: "Wiadomosc numer 2", body: "Zawartosc wiadomosci numer 2 ..." }
+]
 
 setInterval(() => {
   const index = data.length + 1;
   data.push({
     id: index,
     title: `Wiadomosc numer ${index}`,
-    body: `Zawartosc wiadomosci numer ${index} ...`,
+    body: `Zawartosc wiadomosci numer ${index} ...`
   });
-}, 2000);
+  // console.log(data);
+}, 5000);
 
 class App extends Component {
   state = {
     comments: [...data],
   };
 
+    getData = () => {
+
+        if(this.state.comments.length !== data.length){
+            console.log("aktualizacja");
+            this.setState({
+                comments: [...data]
+
+            })
+        } else { console.log("dane takie same -nie aktualizuje");}
+    }
+      
+       
+
+    componentDidMount() {
+      this.idI = setInterval(this.getData,1000)
+    }
+
+    componentWillUnmount() {
+      clearInterval(this.idI)
+    }
+
   render() {
-    const comments = this.state.comments.map((comment) => (
-      <div key={comment.id}>
+    const comments = this.state.comments.map(comment => (
+    <div key={comment.id}>
         <h4>{comment.title}</h4>
         <div>{comment.body}</div>
       </div>
     ));
+    // console.log(comments);
     return (
-      <div>
-        <div className="App">Dziala</div>
+      <div className="App">
+        {comments.reverse()}
       </div>
     );
   }
