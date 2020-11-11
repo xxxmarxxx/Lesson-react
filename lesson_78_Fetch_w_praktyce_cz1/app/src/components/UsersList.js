a441b8b834a0d2c91c861fc2ee5bfd90aeb09902
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2020-11-11 18:59:44 +0100

    add map()

--- lesson_78_Fetch_w_praktyce_cz1/app/src/components/UsersList.js
+++ lesson_78_Fetch_w_praktyce_cz1/app/src/components/UsersList.js
@@ -1,9 +1,14 @@
-import React from 'react';
+import React from "react";
 
-const UsersList = () => {
-    return ( 
-        <button>Dodaj 5 uzytkownikow</button>
-     );
-}
- 
+const UsersList = (props) => {
+  console.log(props.users);
+  const users = props.users.map((user) => (
+    <div key={user.login.uuid}>
+      <h4>{`${user.name.title} ${user.name.first} ${user.name.last}`}</h4>
+  <p>{user.email}</p>
+    </div>
+  ));
+  return <ul>{users}</ul>;
+};
+
 export default UsersList;
