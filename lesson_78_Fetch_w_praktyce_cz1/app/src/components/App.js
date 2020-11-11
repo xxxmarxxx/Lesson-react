7af852895a8f5218be8f43dd8af68aa3ac26f993
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2020-11-11 18:57:22 +0100

    update app.js

--- lesson_78_Fetch_w_praktyce_cz1/app/src/components/App.js
+++ lesson_78_Fetch_w_praktyce_cz1/app/src/components/App.js
@@ -1,19 +1,42 @@
-import React, { Component } from 'react';
-import './App.css';
-import Userslist from "./UsersList";
-import ButtonFetchUsers from "./ButtonFetchUsers"
+import React, { Component } from "react";
+import "./App.css";
+import UsersList from "./UsersList";
+import ButtonFetchUsers from "./ButtonFetchUsers";
+
+const API = "https://randomuser.me/api/?results=5";
 
 class App extends Component {
-  state = {  }
-  render() { 
-    return ( 
-<div className="App">
-      <ButtonFetchUsers/>
-      <UsersList/>
+  state = {
+    users: null,
+  };
+  handleDataFetch = () => {
+    // console.log("click");
+    fetch(API)
+    .then(response => {
+      if(response.ok) {
+        return response
+      }
+      throw Error("Blad!!!!")
+    })
+    .then(response =>response.json())
+    .then(data => {
+      console.log(data);
+      this.setState({
+        users: data.results
+      })
+    })
+    .catch(error => console.log(error))
+  }
+  render() {
+
+    const users = this.state.users;
+    return (
+      <div className="App">
+        <ButtonFetchUsers click={this.handleDataFetch}/>
+        {users ? <UsersList users={users}/> : users}
       </div>
-     );
+    );
   }
 }
- 
-export default App;
 
+export default App;
