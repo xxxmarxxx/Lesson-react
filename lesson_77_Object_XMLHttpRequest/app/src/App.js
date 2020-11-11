cbf9279b514c236369a274edbaaace515f01c8c5
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2020-11-11 16:29:37 +0100

    finish lesson 77

--- lesson_77_Object_XMLHttpRequest/app/src/App.js
+++ lesson_77_Object_XMLHttpRequest/app/src/App.js
@@ -1,22 +1,51 @@
-import React, { Component } from 'react';
-import './App.css';
-
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
-  state = { 
+  state = {
+    users: [],
+  };
 
-   }
+  componentDidMount() {
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", "https://jsonplaceholder.typicode.com/users", true);
+    xhr.onload = () => {
+      console.log(xhr.status);
+      if (xhr.status === 200) {
+        const users = JSON.parse(xhr.response);
+        console.log(users);
+        this.setState({ users });
+      }
+      // console.log(typeof xhr.response);
+    };
+    xhr.send(null);
+  }
 
-  render() { 
-    return ( 
-      <>
-      <h1>Lesson 77 XML Http Request</h1>
-      <div className="App">
-          hallo
-    </div>
-    </>
-     );
+  render() {
+    const users = this.state.users.map((user) => (
+      <div className="users" key={user.id}>
+        <h4>{user.name}</h4>
+        <p>
+          <i>Username: </i>
+          {user.username}
+        </p>
+        <p>
+          <i>Email: </i>
+          {user.email}
+        </p>
+        <p>
+          <i>City: </i>
+          {user.address.city}
+        </p>
+      </div>
+    ));
+    return (
+      <div>
+        <h1>Lesson 77 XML Http Request</h1>
+        <div className="App">{users}</div>
+      </div>
+    );
   }
 }
- 
+
 export default App;
