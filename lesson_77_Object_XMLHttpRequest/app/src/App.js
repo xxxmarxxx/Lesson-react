e8459d3e9df3fef6c3c3ca9e7833375da7a049c0
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2020-11-11 14:22:30 +0100

    first commit lesson 77

---
+++ lesson_77_Object_XMLHttpRequest/app/src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import './App.css';
+
+
+class App extends Component {
+  state = { 
+
+   }
+
+  render() { 
+    return ( 
+      <>
+      <h1>Lesson 77 XML Http Request</h1>
+      <div className="App">
+          hallo
+    </div>
+    </>
+     );
+  }
+}
+ 
+export default App;
