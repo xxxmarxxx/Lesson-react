db72d2859270cb0875962e7ce70c5d82a0538750
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2020-11-11 10:20:35 +0100

    first commit lesson 76 JSON and metod fetch

---
+++ lesson_76_JSON_metod_fetch/app/src/components/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+
+
+class App extends Component {
+  state = {  }
+  render() { 
+    return ( 
+      <div className="App">
+            App fetch
+          </div>
+     );
+  }
+}
+ 
+export default App;
