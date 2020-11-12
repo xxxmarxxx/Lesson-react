8f2000bf8bdd54685a0c06f1ad5fe390dc0e3f1f
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2020-11-12 14:55:14 +0100

    first commit lesson 85

---
+++ lesson_85_86_TworzenieStrukturyFormularza/app/src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  state = {};
+
+  render() {
+    return (
+      <>
+      <h1>Lesson 85-86 Formularz z validation</h1>
+    <div className="App">
+      Formularz
+      </div>
+</>
+  )
+}
+}
+
+export default App;
