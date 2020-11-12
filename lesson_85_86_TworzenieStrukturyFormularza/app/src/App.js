c15993105f7883473ad9e4d61960a93812f6054f
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2020-11-12 15:09:54 +0100

    add form, label, input

--- lesson_85_86_TworzenieStrukturyFormularza/app/src/App.js
+++ lesson_85_86_TworzenieStrukturyFormularza/app/src/App.js
@@ -2,14 +2,21 @@ import React, { Component } from "react";
 import "./App.css";
 
 class App extends Component {
-  state = {};
+  state = {
+    username: "",
+  };
 
   render() {
     return (
       <>
       <h1>Lesson 85-86 Formularz z validation</h1>
     <div className="App">
-      Formularz
+      <form>
+        <label htmlFor="user"> Twoje imie: 
+          <input type="text" id="user" name="username" value={this.state.username}
+          onChange={this.handleChange}/>
+        </label>
+      </form>
       </div>
 </>
   )
