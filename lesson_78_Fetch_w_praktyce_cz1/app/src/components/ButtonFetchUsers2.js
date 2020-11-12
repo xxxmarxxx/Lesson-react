9ccfb0fb71c98a5e7566cc5fe691b008a6249ac3
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2020-11-12 11:15:22 +0100

    dodadne do cz.2 dodaj uzytkownika

---
+++ lesson_78_Fetch_w_praktyce_cz1/app/src/components/ButtonFetchUsers2.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ButtonFetchUsers = (props) => {
+  return <button onClick={props.click}>Dodaj 5 uzytkownikow</button>;
+};
+
+export default ButtonFetchUsers;
---
+++ lesson_78_Fetch_w_praktyce_cz1/app/src/components/UsersList2.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const UsersList = (props) => {
+  console.log(props.users);
+  const users = props.users.map((user) => (
+    <div key={user.login.uuid}>
+      <img src={user.picture.large} alt={user.name.last} />
+      <h3>{`${user.name.title} ${user.name.first} ${user.name.last}`}</h3>
+      <p>-----</p>
+      <p>
+        {user.location.city}/{user.location.country}
+      </p>
+      <p>
+        {user.location.street.name} No. {user.location.street.number}
+      </p>
+      <p>{user.email}</p>
+    </div>
+  ));
+  return <ul>{users}</ul>;
+};
+
+export default UsersList;
