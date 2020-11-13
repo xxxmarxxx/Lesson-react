d3a5055191ac006ddd33b393af8a08c000edbbbf
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2020-11-13 11:29:54 +0100

    add const tasks

--- lesson_90_ToDo/app/src/components/TaskList.js
+++ lesson_90_ToDo/app/src/components/TaskList.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import Task from './Task'
 
-const TaskList = () => {
+const TaskList = (props) => {
+
+    const tasks = props.tasks.map(task => <Task key={task.id} task={task} />)
+
     return ( 
-        <div>
-           <h2>Lista Taskow</h2> 
-           <Task />
-           <Task />
-           <Task />
+        <>
+        <div className="active">
+           <h2>Zadania do zrobienia</h2> 
+           {tasks}
         </div>
+         <hr/>
+         <div className="active">
+           <h3>Zadania zrobione (0)</h3> 
+        </div>
+        </>
      );
 }
  
