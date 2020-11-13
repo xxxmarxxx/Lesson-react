34b03a7f09655d3bfbefff61a90c2e20f65e0a9b
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2020-11-13 16:50:20 +0100

    finish lesson 98 add input, button

--- lesson_90_ToDo/app/src/components/AddTask.js
+++ lesson_90_ToDo/app/src/components/AddTask.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
 
 class AddTask extends Component {
-    state = {  }
+    state = { 
+        text: '',
+        checked: false,
+        date: "2020-11-13",
+     }
     render() { 
         return ( 
             <div className="form">
-            <div>Dodaj task</div>
-            <br/>
-            <hr/>
+            <input type="text" placeholder="dodaj zadanie" value={this.state.text} />
+            <input style={{marginLeft: 10}} type="checkbox" checked={this.state.checked} id="important"/> 
+            <label style={{margin: 10}} htmlFor="important">Priorytet</label><br/><br/>
+            <label style={{margin: 10}} htmlFor="date">Do kiedy zrobic</label>
+            <input type="date" value={this.state.date} min="2020-11-01" max="2021-02-31"/><br/>
+            <button>Dodaj</button>
             </div>
          );
     }
