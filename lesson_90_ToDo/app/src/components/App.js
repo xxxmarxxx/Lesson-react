3ce1433ae6cd9174e608b8c0c19d4eb1a007dad7
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2020-11-13 19:05:49 +0100

    finish lesson 98

--- lesson_90_ToDo/app/src/components/App.js
+++ lesson_90_ToDo/app/src/components/App.js
@@ -92,7 +92,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Lesson 90-96 - ToDo App</h1>
+        <h1>Lesson 90-98 - ToDo App</h1>
         <h2 style={{margin: 20}}>ToDo APP</h2>
         <AddTask />
         <TaskList tasks={this.state.tasks} delete={this.deleteTask}
