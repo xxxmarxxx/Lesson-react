c76f6b374aaade89db37cf23ee93ce3bbd2ac669
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2020-11-13 10:32:25 +0100

    create TaskList

---
+++ lesson_90_ToDo/app/src/components/TaskList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Task from './Task'
+
+const TaskList = () => {
+    return ( 
+        <div>
+           <h2>Lista Taskow</h2> 
+           <Task />
+        </div>
+     );
+}
+ 
+export default TaskList;
