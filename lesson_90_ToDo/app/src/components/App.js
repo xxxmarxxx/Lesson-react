9bb34cefc1daf3e69afbb44c48f176da2907266a
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2020-11-13 10:29:01 +0100

    first commit lesson 90 ToDo

---
+++ lesson_90_ToDo/app/src/components/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './App.css';
+
+
+
+class App extends Component {
+  state = {  }
+  render() { 
+    return ( 
+<div className="App">
+  <h1>Lesson 90 - ToDo App</h1>
+ 
+    </div>
+     );
+  }
+}
+ 
+export default App;
