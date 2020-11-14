c7ea38d0761b26645900e2a49322298d87ca74f1
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2020-11-14 13:59:55 +0100

    finish lesson 100 metoda sort()

--- lesson_90_ToDo/app/src/components/TaskList.js
+++ lesson_90_ToDo/app/src/components/TaskList.js
@@ -4,6 +4,34 @@ import Task from "./Task";
 const TaskList = (props) => {
   const active = props.tasks.filter((task) => task.active);
   const done = props.tasks.filter((task) => !task.active);
+
+  // metoda 1
+  // done.sort((a,b) => { return b.finishDate - a.finishDate })
+
+  if(done.length >= 2 ){
+    done.sort((a,b) => {
+      if (a.finishDate < b.finishDate){
+        return 1
+      }
+      if (a.finishDate > b.finishDate){
+        return -1
+      }
+      return 0
+    })
+  }
+
+  if(active.length > 2){
+    active.sort((a,b) => {
+
+      a = a.text.toLowerCase();
+      b = b.text.toLowerCase();
+
+if(a.text < b.text) return -1;
+if(a.text > b.text) return 1;
+return 0
+    })
+  }
+
 //   console.log(active, done);
 
   const activeTasks = active.map((task) => (
