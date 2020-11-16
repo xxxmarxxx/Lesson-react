1a199333f843495a35eccbcde8e75fd208b52feb
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2020-11-16 15:23:08 +0100

    add Router

--- lesson_106_project2_SPA/app/src/layouts/App.js
+++ lesson_106_project2_SPA/app/src/layouts/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 import '../styles/App.css';
+import {BrowserRouter as Router} from 'react-router-dom';
 
 class App extends Component {
   state = {  }
   render() { 
     return ( 
+      <Router>
       <div className="App">
         SPA
       </div>
+      </Router>
      );
   }
 }
