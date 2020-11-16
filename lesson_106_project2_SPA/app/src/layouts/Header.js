877743eb589b7c30b6a4d09148cc605fe21954e8
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2020-11-16 20:10:34 +0100

    finish lesson 109 Math.random() nie dziala

--- lesson_106_project2_SPA/app/src/layouts/Header.js
+++ lesson_106_project2_SPA/app/src/layouts/Header.js
@@ -1,13 +1,44 @@
 import React from "react";
+import { Route, Switch } from 'react-router-dom'
 import image1 from "../images/image1.png"
 import image2 from "../images/image2.png"
 import image3 from "../images/image3.png"
 import '../styles/Header.css';
 
 const Header = () => {
-  return <div>
-    <img src={image1} alt="image1"/>
-    </div>;
-};
+
+  // nie dziala
+  // const images = [image1,image2,image3];
+  // const index = Math.floor(Math.random()*3)
+  // const image = images[index];
+
+  return (
+    <>
+    
+    <Switch>
+    <Route path="/" exact render={() => (
+      <img src={image3} alt="image1"/>
+    )}/>
+    <Route path="/products" render={() => (
+      <img src={image1} alt="image1"/>
+    )}/>
+    <Route path="/contact" render={() => (
+      <img src={image2} alt="image2"/>
+    )}/>
+    <Route path="/admin" render={() => (
+      <img src={image3} alt="image3"/>
+    )}/>
+    <Route render={() => (
+      <img src={image1} alt="image1"/>
+    )}/>
+    </Switch>
+
+   {/* <img src={image} alt="header"/> */}
+
+    </>
+
+  );
+  
+}
 
 export default Header;
