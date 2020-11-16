2860350c968c7e4e2c2dfa9e0d8912cef0f2f086
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2020-11-16 18:43:46 +0100

    add images to Header.js

--- lesson_106_project2_SPA/app/src/layouts/Header.js
+++ lesson_106_project2_SPA/app/src/layouts/Header.js
@@ -1,7 +1,13 @@
 import React from "react";
+import image1 from "../images/image1.png"
+import image2 from "../images/image2.png"
+import image3 from "../images/image3.png"
+import '../styles/Header.css';
 
 const Header = () => {
-  return <div>Header</div>;
+  return <div>
+    <img src={image1} alt="image1"/>
+    </div>;
 };
 
 export default Header;
