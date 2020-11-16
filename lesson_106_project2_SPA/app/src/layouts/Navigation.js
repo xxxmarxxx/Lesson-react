24c81dfd530e31306af31f77efe8c7241d1534b3
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2020-11-16 16:57:29 +0100

    create const list

--- lesson_106_project2_SPA/app/src/layouts/Navigation.js
+++ lesson_106_project2_SPA/app/src/layouts/Navigation.js
@@ -1,7 +1,29 @@
 import React from "react";
+import {NavLink} from 'react-router-dom';
+import "../styles/Navigation.css";
 
-const Navigation = () => {
-  return <div>Nav</div>;
+const list = [
+  {name: "start",path: "/"},
+  {name: "produkty",path: "/products"},
+  {name: "Kontakt",path: "/contact"},
+  {name: "panel admin",path: "/admin"},
+]
+
+const Navigation = ()=> {
+
+  const menu = list.map(item => (
+  <li key={item.name}>
+    <NavLink to={item.path}>{item.name}</NavLink>
+  </li>
+))
+
+  return (
+  <nav>
+    <ul>
+    {menu}
+    </ul>‚
+  </nav>
+  )
 };
 
 export default Navigation;
