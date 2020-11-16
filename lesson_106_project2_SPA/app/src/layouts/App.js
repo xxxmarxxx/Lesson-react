b7d5ce51d28e0feedabb22ba350a5debf951bcee
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2020-11-16 16:15:18 +0100

    create web structure

--- lesson_106_project2_SPA/app/src/layouts/App.js
+++ lesson_106_project2_SPA/app/src/layouts/App.js
@@ -1,18 +1,34 @@
-import React, { Component } from 'react';
-import '../styles/App.css';
-import {BrowserRouter as Router} from 'react-router-dom';
+import React, { Component } from "react";
+import "../styles/App.css";
+import { BrowserRouter as Router } from "react-router-dom";
+import Header  from "./Header";
+import Navigation from "./Navigation";
+import Page from "./Page";
+import Footer from './Footer';
 
 class App extends Component {
-  state = {  }
-  render() { 
-    return ( 
+  state = {};
+  render() {
+    return (
       <Router>
-      <div className="App">
-        SPA
-      </div>
+        <div className="app">
+          <header>
+            {<Header/>}
+            </header>
+          <main>
+            <aside>{<Navigation/>}</aside>
+            <section className="page">
+              {<Page/>}
+              </section>
+          </main>
+          <footer>
+                {<Footer/>}
+              </footer>
+          
+        </div>
       </Router>
-     );
+    );
   }
 }
- 
+
 export default App;
