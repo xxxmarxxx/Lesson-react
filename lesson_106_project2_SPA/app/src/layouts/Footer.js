c073cfe081561e1722dcee30605ddcecf3341263
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2020-11-17 17:16:05 +0100

    upgrade footer.js and finish lesson 115

--- lesson_106_project2_SPA/app/src/layouts/Footer.js
+++ lesson_106_project2_SPA/app/src/layouts/Footer.js
@@ -1,7 +1,50 @@
 import React from "react";
+import { Route } from "react-router-dom";
+import "../styles/Footer.css";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div>
+      <h3>Footer</h3>
+      <Route
+        path="/"
+        exact
+        render={(props) => (
+            <p>Jestes na <span>stronie glownej</span></p>
+            )
+        }
+      />
+
+      <Route
+        path="/:page"
+        exact
+        render={(props) => {
+          console.log(props);
+          return (
+            <>
+          <p>Jestes na <span>{props.match.params.page}</span></p>
+          <p>Jestes na <span>{props.match.url}</span></p>
+          <p>Jestes na <span>{props.match.path}</span></p>
+          </>
+          );
+        }}/>
+
+
+<Route
+        path="/:page/:idProduct"
+        exact
+        render={(props) => {
+          console.log(props);
+          return (
+            <>
+          <p>Jestes na <span>{props.match.params.idProduct}</span></p>
+          <p>Jestes na <span>{props.match.url}</span></p>
+          <p>Jestes na <span>{props.match.path}</span></p>
+          </>
+          );
+        }}/>
+    </div>
+  );
 };
 
 export default Footer;
