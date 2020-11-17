b9ac49c4bb25bbfac29a60c3f0cb983d0943f4a2
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2020-11-17 13:38:58 +0100

    upgrade ContactPage finish lesson 112

--- lesson_106_project2_SPA/app/src/pages/ContactPage.js
+++ lesson_106_project2_SPA/app/src/pages/ContactPage.js
@@ -1,10 +1,46 @@
-import React from 'react';
+import React from "react";
+import '../styles/ContactPage.css';
+import {Prompt} from 'react-router-dom'
 
+class ConctactPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: "",
+      
+    };
+  }
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.setState({
+        value: ""
+    })
+  };
+  
+  handleChange = (e) => {
+    this.setState({
+      value: e.target.value,
+    });
+  };
 
-const ContactPage = () => {
-    return ( 
-        <div>Kontakt strona</div>
-     );
+  render() {
+    return (
+      <div className="contact">
+        <form onSubmit={this.handleSubmit}>
+          <h3>Napisz do nas</h3>
+          <textarea
+            value={this.state.value}
+            onChange={this.handleChange}
+            placeholder="wpisz wiadomosc"
+          ></textarea>
+          <button>Wyslij</button>
+        </form>
+        <Prompt
+        when={this.state.value}
+        message="masz nie wypelniony formularz, czy chcesz opuscic te strone" />
+      </div>
+    );
+  }
 }
- 
-export default ContactPage;
+
+export default ConctactPage;
