19d443f1b186ee52923601cb573497e421b85a71
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2020-11-17 16:02:36 +0100

    add import {Route,Redtect} , const permission

--- lesson_106_project2_SPA/app/src/pages/AdminPage.js
+++ lesson_106_project2_SPA/app/src/pages/AdminPage.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import {Route, Redirect} from 'react-router-dom';
 
+const permission = true; //false wsietla panel 
 
 const AdminPage = () => {
     return ( 
-        <div>Admin</div>
+       <Route render={()=> (
+           permission ? (<h3>Panel admina - dziendobry</h3>) : (
+               <Redirect to="/login" />
+           )
+       )} />
      );
 }
  
