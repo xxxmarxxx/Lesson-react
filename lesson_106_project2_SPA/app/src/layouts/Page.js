7ff00fdeb9ed17658ed4f1378d2d31484d4248eb
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2020-11-17 16:01:15 +0100

    add path login, product/:id and import

--- lesson_106_project2_SPA/app/src/layouts/Page.js
+++ lesson_106_project2_SPA/app/src/layouts/Page.js
@@ -3,8 +3,10 @@ import { Route, Switch } from "react-router-dom";
 import HomePage from '../pages/HomePage';
 import ContactPage from '../pages/ContactPage';
 import ProductPage from '../pages/ProductPage';
+import ProductListPage from '../pages/ProductListPage';
 import AdminPage from '../pages/AdminPage';
 import ErrorPage from '../pages/ErrorPage';
+import LoginPage from '../pages/LoginPage';
 
 
 const Page = () => {
@@ -12,8 +14,10 @@ const Page = () => {
 <>
 <Switch>
     <Route path="/" exact component={HomePage} />
-    <Route path="/products"  component={ProductPage} />
+    <Route path="/products" component={ProductListPage} />
+    <Route path="/product/:id" component={ProductPage} />
     <Route path="/contact" component={ContactPage} />
+    <Route path="/login"  component={LoginPage} />
     <Route path="/admin"  component={AdminPage} />
     <Route component={ErrorPage} />
     </Switch>
