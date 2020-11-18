1719fe85b0d26c63116f8dc81e02721bb47a68ec
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2020-11-18 16:48:21 +0100

    add state underline, const text, let classes lesson 118

--- lesson_116_Style_linowe/app/src/App2.js
+++ lesson_116_Style_linowe/app/src/App2.js
@@ -1,19 +1,24 @@
-import React from 'react';
-import './App.css';
-
+import React from "react";
+import "./App.css";
+import Text from "./Text";
 
 class App extends React.Component {
   state = {
-  }
-  render() { 
-    
-return ( 
-      <div>
-   <h1>Witaj na stronie</h1>
-      </div>
-     );
+      underline: false
+  };
+  render() {
+      const text = "Witaj na stronie"
+      let classes = "";
+      if(this.state.underline) classes+=" line"
+      if(text.length > 10) classes += " big"
 
+    return (
+      <div className="app" onClick={()=> this.setState({underline : !this.state.underline })}>
+        <h1 className={classes}>{text}</h1>
+        <Text />
+      </div>
+    );
   }
 }
- 
+
 export default App;
