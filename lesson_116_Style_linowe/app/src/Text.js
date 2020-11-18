7c78a6349be6e1131ac527289e24ad1715623eb7
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2020-11-18 16:47:03 +0100

    create text.js lesson 118

---
+++ lesson_116_Style_linowe/app/src/Text.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Text.css';
+
+
+const Text = () => {
+    return ( 
+        <p className="red">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusantium eius sapiente deleniti consequuntur nostrum pariatur. Dicta reprehenderit fugit quidem quia aut, expedita incidunt commodi tempore fugiat repellendus dolor corporis error?
+        </p>
+     );
+
+}
+ 
+export default Text;
