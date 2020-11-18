cbf06ad2e855f0b7a310adb6feeba80d4f152436
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2020-11-18 14:14:30 +0100

    lesson 116 -  3 versions on  styling

--- lesson_116_Style_linowe/app/src/App.js
+++ lesson_116_Style_linowe/app/src/App.js
@@ -7,21 +7,90 @@ class App extends React.Component {
     active: false
   }
   render() { 
+    /////////////////// wersja 1
+    // const btn_off = {
+    //   border: '2px solid black',
+    //   borderRadius: '10px',
+    //   padding: '15.4px 32.1px 15px',
+    //   fontSize: '30px',
+    //   fontFamily: 'arial',
+    //   display: 'block',
+    //   margin: '20px auto',
+    //   backgroundColor: '#fff'
+    // }
 
-    const btn_off = {
-      border: '2px solid black',
-      padding: '15.4px 32.1px 15px',
-      fontFamily: 'arial',
-      display: 'block',
-      margin: '20px auto',
-      backgroundColor: '#fff'
-    }
+    // const btn_on = {
+    //   border: '2px solid black',
+    //   borderRadius: '10px',
+    //   padding: '15.4px 32.1px 15px',
+    //   fontSize: '30px',
+    //   fontFamily: 'arial',
+    //   display: 'block',
+    //   margin: '20px auto',
+    //   backgroundColor: 'cadetblue',
+    //   color: '#fff',
+    // }
     
-    return ( 
+    // return ( 
+    //   <div>
+    // <button style={this.state.active ? btn_on : btn_off} onClick={()=> this.setState({ active: !this.state.active })}>{this.state.active ? "Wylacz" : "Wlacz"}</button>
+    //   </div>
+    //  );
+    ////////////////
+    //////////////// wersja 2 const style
+    // const style = {
+    //   btn_off: {
+    //     border: '2px solid black',
+    //     borderRadius: '10px',
+    //     padding: '15.4px 32.1px 15px',
+    //     fontSize: '30px',
+    //     fontFamily: 'arial',
+    //     display: 'block',
+    //     margin: '20px auto',
+    //     backgroundColor: '#fff'
+    //   },
+    //   btn_on: {
+    //     border: '2px solid black',
+    //     borderRadius: '10px',
+    //     padding: '15.4px 32.1px 15px',
+    //     fontSize: '30px',
+    //     fontFamily: 'arial',
+    //     display: 'block',
+    //     margin: '20px auto',
+    //     backgroundColor: 'cadetblue',
+    //     color: '#fff'
+    //   }
+    // };
+
+    // return ( 
+    //   <div>
+    // <button style={this.state.active ? style.btn_on : style.btn_off} onClick={()=> this.setState({ active: !this.state.active })}>{this.state.active ? "Wylacz" : "Wlacz"}</button>
+    //   </div>
+    //  );
+
+
+    ////////////// wersja 3 let
+  let btn = {
+        border: '2px solid black',
+        borderRadius: '10px',
+        padding: '15.4px 32.1px 15px',
+        fontSize: '30px',
+        fontFamily: 'arial',
+        display: 'block',
+        margin: '20px auto',
+        backgroundColor: '#fff'
+  }
+  if (this.state.active){
+  btn.backgroundColor = 'cadetblue';
+  btn.color = '#fff';
+  }
+
+return ( 
       <div>
-    <button style={btn_off} onClick={()=> this.setState({ active: !this.state.active })}>{this.state.active ? "Wylacz" : "Wlacz"}</button>
+    <button style={btn} onClick={()=> this.setState({ active: !this.state.active })}>{this.state.active ? "Wylacz" : "Wlacz"}</button>
       </div>
      );
+
   }
 }
  
