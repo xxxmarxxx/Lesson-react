fb05d1c856981044cab2d10e84383302d5e6ac11
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2020-11-18 17:22:03 +0100

    finish lesson 118

--- lesson_116_Style_linowe/app/src/App2.js
+++ lesson_116_Style_linowe/app/src/App2.js
@@ -1,23 +1,31 @@
 import React from "react";
 import "./App.css";
 import Text from "./Text";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends React.Component {
   state = {
-      underline: false
+    underline: false,
   };
   render() {
       const text = "Witaj na stronie"
-      let classes = "";
-      if(this.state.underline) classes+=" line"
-      if(text.length > 10) classes += " big"
+    //   let classes = "";
+    //   if(this.state.underline) classes+=" line"
+    //   if(text.length > 10) classes += " big"
 
-    return (
-      <div className="app" onClick={()=> this.setState({underline : !this.state.underline })}>
-        <h1 className={classes}>{text}</h1>
-        <Text />
-      </div>
-    );
+    const classes = ['one'];
+    if (this.state.underline) classes.push('line');
+    if (text.length > 10) classes.push('big')
+
+      return (
+        <div
+          className="app"
+          onClick={() => this.setState({ underline: !this.state.underline })}
+        >
+          <h1 className={classes.join(" ")}>{text}</h1>
+          <Text />
+        </div>
+      );
   }
 }
 
--- lesson_116_Style_linowe/app/src/Text.js
+++ lesson_116_Style_linowe/app/src/Text.js
@@ -1,14 +1,20 @@
-import React from 'react';
-import './Text.css';
-
+import React from "react";
+import "./Text.css";
 
 const Text = () => {
-    return ( 
-        <p className="red">
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusantium eius sapiente deleniti consequuntur nostrum pariatur. Dicta reprehenderit fugit quidem quia aut, expedita incidunt commodi tempore fugiat repellendus dolor corporis error?
-        </p>
-     );
+  return (
+    <div>
+      <button className="btn btn-primary">
+        Click me
+      </button>
+      <p className="red">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusantium
+        eius sapiente deleniti consequuntur nostrum pariatur. Dicta
+        reprehenderit fugit quidem quia aut, expedita incidunt commodi tempore
+        fugiat repellendus dolor corporis error?
+      </p>
+    </div>
+  );
+};
 
-}
- 
 export default Text;
