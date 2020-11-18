2af493e8a6823507cd90aacb5eb53ab09e3f4f42
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2020-11-18 14:32:41 +0100

    first commit  lesson 117

--- lesson_116_Style_linowe/app/README.md
+++ lesson_116_Style_linowe/app/README.md
@@ -1,6 +1,7 @@
 # Style React with Css
 
-
+App = lesson 116 - inline style,
+App2 = lesson 117 - with file app.css
 
 ## inline style
 
---
+++ lesson_116_Style_linowe/app/src/App2.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+
+
+class App extends React.Component {
+  state = {
+  }
+  render() { 
+    
+return ( 
+      <div>
+   <h1>Witaj na stronie</h1>
+      </div>
+     );
+
+  }
+}
+ 
+export default App;
